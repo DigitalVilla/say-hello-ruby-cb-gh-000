0af2f7fa5acbf4e4041d4d30caf5a2dcfcd8f158
Omar Villa <o.villanueva@thearcane.com>
2020-06-14 02:49:11 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,4 +1,8 @@
 # Build your say_hello method here
 def say_hello(name)
-  return "Hello #{name}!"
+  unless name
+    return puts "Hello #{name}!"
+  else
+    return puts "Hello Ruby Programmer!"
+  end
 end
